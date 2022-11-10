e3624fee3cefe483add57d5aafcd0876701f74df
XieJiSS <XieJiSS@users.noreply.github.com>
2022-11-10 12:45:32 +0800

    chore: fix types

--- stats.js
+++ stats.js
@@ -7,6 +7,9 @@ const { alpmVercmp, versionRegex } = require("./utils/alpm");
 
 const highlightPkgs = require("./config.highlight");
 
+/**
+ * @param {Date} date
+ */
 const yyyymmdd = (date) => {
   const mm = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() is zero-based
   const dd = String(date.getDate()).padStart(2, "0");
